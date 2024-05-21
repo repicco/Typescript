52d261490d3456f6a118d6d5a344092a93b04098
renato.picco.3.glo <renato.picco.3.glo>
2024-05-21 00:12:14 -0300

    starting with type script

--- projeto1/aula1.js
+++ projeto1/aula1.js
@@ -1,2 +1,10 @@
+//ecmascript 3
 var nome = 'Renato';
 console.log('Bem vindo ' + nome);
+var Sistema = /** @class */ (function () {
+    function Sistema(ip) {
+        this.ip = ip;
+    }
+    return Sistema;
+}());
+var servidor1 = new Sistema('192.168.500.3');
--- projeto1/aula1.ts
+++ projeto1/aula1.ts
@@ -3,3 +3,13 @@
 const nome = 'Renato';
 
 console.log('Bem vindo ' + nome)
+
+class Sistema {
+    ip: string;
+
+    constructor(ip: string){
+        this.ip = ip
+    }
+}
+
+const servidor1 = new Sistema('192.168.500.3')
